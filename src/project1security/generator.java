8a640b4a1fbb3ac888043de5602cbc108106d014
Miranda Neville <mirandaneville_2016@depauw.edu>
2015-11-16 20:41:11 -0500

    a work in progress

--- src/project1security/generator.java
+++ src/project1security/generator.java
@@ -2,9 +2,18 @@ package project1security;
 
 public class generator 
 {
+	 public static void main(String[] args) 
+	 {
+	char[] keyList; 
 	
-	
-	
+	keyList = new char[26];
+	keyList[0] = '@';
+	keyList[1] = 'b';
+	keyList[2] = 'c';
+	keyList[3] = 'd';
+	keyList[4] = '3';
+	keyList[5] = 'f';
 	
 
+}
 }
