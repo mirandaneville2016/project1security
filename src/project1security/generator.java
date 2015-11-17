a9de26bb270511d65bc3f2da94ab59d09c981424
Miranda Neville <mirandaneville_2016@depauw.edu>
2015-11-16 20:56:58 -0500

    array filled

--- src/project1security/generator.java
+++ src/project1security/generator.java
@@ -13,6 +13,26 @@ public class generator
 	keyList[3] = 'd';
 	keyList[4] = '3';
 	keyList[5] = 'f';
+	keyList[6] = 'g';
+	keyList[7] = '4';
+	keyList[8] = '!';
+	keyList[9] = 'j';
+	keyList[10] = 'k';
+	keyList[11] = '7';
+	keyList[12] = 'm';
+	keyList[13] = 'n';
+	keyList[14] = '0';
+	keyList[15] = 'p';
+	keyList[16] = 'q';
+	keyList[17] = 'r';
+	keyList[18] = '5';
+	keyList[19] = '+';
+	keyList[20] = 'u';
+	keyList[21] = 'v';
+	keyList[22] = 'w';
+	keyList[23] = '*';
+	keyList[24] = 'y';
+	keyList[25] = 'z';
 	
 
 }
