7f3e233c4768c3f61a1fd02b63af4da0afdf92ab
chris <chris@DESKTOP-HGTH3SN.depauw.edu>
2015-11-19 18:10:05 -0500

    Miranda did it

--- src/project1security/generator.java
+++ src/project1security/generator.java
@@ -1,14 +1,21 @@
 package project1security;
 import java.util.*;
+import java.io.*;
 
 public class generator 
 {
-	 
-	private final byte[] keyList = new byte[]{'@','b','c','d','3','f','g','4',
-			'!','j','k','7','m','n','0','p','q','r','5','+','u','v','w','*','y','z'};
-	
-	
-	
+     
+    private final byte[] keyList = new byte[]{'@','b','c','d','3','f','g','4',
+            '!','j','k','7','m','n','0','p','q','r','5','+','u','v','w','*','y','z'};
+    
+    public static void main(String args[])
+    {
+        String use = User.getUsername();
+        String site = siteName;
+        
+        
+    }
+    
 
 
 }
