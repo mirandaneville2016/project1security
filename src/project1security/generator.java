9e908df0367b24d940320b26338e7b30210939e8
Miranda Neville <mirandaneville_2016@depauw.edu>
2015-11-20 08:52:22 -0500

    work in progress
    testing

--- src/project1security/generator.java
+++ src/project1security/generator.java
@@ -1,15 +1,15 @@
 package project1security;
 import java.util.*;
+import java.io.*;
 
 public class generator 
 {
 	public String use;
+	public String si;
 	 
-	private final byte[] keyList = new byte[]{'@','b','c','d','3','f','g','4',
-			'!','j','k','7','m','n','0','p','q','r','5','+','u','v','w','*','y','z'};
-	
+	private final byte[] keyList = new byte[]{'@','b','c','d','3','f','g','4','!','j','k','7','m','n','0','p','q','r','5','+','u','v','w','*','y','z'};
 	use = User.getUsername();
-	byte[]getBytes(use ,charset);
-
+	si = siteName;
+    
 
 }
