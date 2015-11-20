d836962eb7abae7564da149c26a8e81c81ad63a7
mirandaneville2016 <mirandaneville_2016@depauw.edu>
2015-11-20 11:18:17 -0500

    closer but still to far

--- src/project1security/generator.java
+++ src/project1security/generator.java
@@ -2,30 +2,39 @@ package project1security;
 import java.util.*;
 import java.io.*;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 public class generator 
 {
-<<<<<<< HEAD
-	public String use;
-	public String si;
+
+	private static final String Google = null;
+	public String siteName = Google;
+	public String use = User.getUsername();
+	public String si = siteName;
+	
+	
 	 
 	private final byte[] keyList = new byte[]{'@','b','c','d','3','f','g','4','!','j','k','7','m','n','0','p','q','r','5','+','u','v','w','*','y','z'};
-	use = User.getUsername();
-	si = siteName;
+	
+	private Array newName(String u,String s)
+	{
+		byte[] temp;
+		byte[] newName;
+		int h = u.length()/2;
+		for (int i = 0; i < u.length(); i++)
+		{
+			if (i<h)
+				
+			
+		}
+		
+		
+	}
     
-=======
-     
-    private final byte[] keyList = new byte[]{'@','b','c','d','3','f','g','4',
-            '!','j','k','7','m','n','0','p','q','r','5','+','u','v','w','*','y','z'};
-    
-    public static void main(String args[])
-    {
-        String use = User.getUsername();
-        String site = siteName;
-        
-        
-    }
-    
-
->>>>>>> origin/master
-
 }
+     
+    
+
+
+
+
