7e07d898c97f43609850cb1140df25c4c822fff1
chris_n2tmxwz <chris_n2tmxwz@DESKTOP-NBC63QD>
2015-11-22 12:51:49 -0500

    almost there

--- src/project1security/generator.java
+++ src/project1security/generator.java
@@ -16,22 +16,30 @@ public class generator
 	 
 	private final byte[] keyList = new byte[]{'@','b','c','d','3','f','g','4','!','j','k','7','m','n','0','p','q','r','5','+','u','v','w','*','y','z'};
 	
-	private Array newName(String u,String s)
+	public String newName(String u,String s)
 	{
-		byte[] temp;
-		byte[] newName;
+		byte[] temp = null;
+		byte[] newName = null;
 		int h = u.length()/2;
 		for (int i = 0; i < u.length(); i++)
 		{
 			if (i<h)
-				temp[i]= u.atChar(i);
+				temp[i]= (byte) u.charAt(i);
+			else 
+				newName[i] = (byte) u.charAt(i);
 				
-			
 		}
-		
-		
-	}
+		for (int k = 0, j = newName.length; k < s.length(); k ++, j++)
+		{
+			newName[j] = (byte) s.charAt(k);
+		}
+		for (int p = 0, t = newName.length; p <= temp.length; p ++, t ++)
+		{
+			newName[t] = temp [p];
+		}
     
+	}
+	
 }
      
     
