24046b01336ba81a0e16acd49c50e3dec7653fb6
chris <christophernuss@depauw.edu>
2015-11-30 20:37:50 -0500

    work in progress still lol but hey I can push and pull again

--- src/project1security/Generator.java
+++ src/project1security/Generator.java
@@ -6,14 +6,20 @@ import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 public class Generator 
 {
-	
+
+	//* below array creates the key to create the password//
+		private final byte[] keyList = new byte[] {'@','b','c','d','3','f','g','4','!','j','k','7','m','n','0','p','q','r','5','+','u','v','w','*','y','z'};
+		private final byte[] alpha= new byte[] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+		private final byte[] caps = new byte[] {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};  
+
 	
 	
 	// this method is where all of the cool stuff happens, splits the username in two then puts last half in front followed by 
 	// the sitename followed by the last half of the username.  Then runs it against the key
-	public String securepword(String u,String s){
-try	{	
-		String securepword = null;
+	public String securepword(String u,String s)
+	{
+		
+		String pword = "";
 		byte[] temp = null;
 		byte[] newName = null;
 		int h = u.length()/2;
@@ -57,18 +63,20 @@ try	{
 			{
 				pw[pw.length] = '@';
 			}
-			securepword = pw.toString();
+			pword = pw.toString();
 		}
-		if (securepword.length()< 9)
+		if (pword.length()< 9)
 		{
-			 int hm = 9 - securepword.length();
+			 int hm = 9 - pword.length();
 			 while (hm < 0)
 			 {
-				 securepword = securepword + "S";
+				 pword = pword + "S";
 			 }
 		}
-		else{
-		return securepword;}
+		
+		
+		return pword;
+		
 				
     
 	}
@@ -84,16 +92,11 @@ try	{
 			
 		}
 		return yon;
-	}throw (NullPointerException);
+	}
 	
 }
-}
      
- //* below array creates the key to create the password//
-	private final byte[] keyList = new byte[] {'@','b','c','d','3','f','g','4','!','j','k','7','m','n','0','p','q','r','5','+','u','v','w','*','y','z'};
-	private final byte[] alpha= new byte[] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-	private final byte[] caps = new byte[] {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};}   
-
+ 
 
 
 
